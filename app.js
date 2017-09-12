93e0dc53ca210bcc70cf46a72d74318099073916
Fradeira Guillaume <guillaumefradeira@yahoo.fr>
2017-09-12 17:50:23 +0200

    Init

---
+++ app.js
@@ -0,0 +1,25 @@
+var builder = require('botbuilder');
+var restify = require('restify');
+
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url); 
+ });
+ 
+var connector = new builder.ChatConnector({
+    appId: process.env.APP_ID,
+    appPassword: process.env.APP_PASSWORD
+});
+ 
+server.post('/api/messages', connector.listen()); 
+
+var bot = new builder.UniversalBot(connector, function (session) {
+
+    
+
+    // bot.on('typing', function(){
+    //     session.send(`haha, t'es en train d'ecrire :)`);
+    // }) 
+    
+});
+
